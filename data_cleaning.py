62089bbee2c47b4e99482902182ddccad7ee799b
elfysh <144109386+elfysh@users.noreply.github.com>
2024-05-11 23:31:04 +0300

    Adding data_cleaning

---
+++ data_cleaning.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Удаляем фильмы, в которых хотя бы 1 параметр неизвестен
+df = pd.read_json('movies_data_v3.json')
+df = df.dropna()
+
+# Оставляем только одного режиссера у фильма
+df['director'] = df['director'].str.split(',').str.get(0)
+
+# Форматируем жанры
+df['genre'] = df['genre'].str.split('|').apply(lambda x: ','.join([genre.capitalize() for genre in x]))
+print(df)
